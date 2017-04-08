b84515cd1cfd579a9364277b7094fd6ae70b1601
anastasia1296 <ivanko.anastasia@gmail.com>
2017-04-08 16:29:39 +0300

    some bad coomit with idea files

---
+++ src/Task2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by Anastasya on 08.04.2017.
+ */
+public class Task2 {
+    public static void main(String []args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input three numbers");
+        int n = scan.nextInt();
+        int h = scan.nextInt();
+        int k = scan.nextInt();
+        if ( n>h){
+            System.out.println(n);
+
+        }else{
+            System.out.println(n+","+k);
+        }
+    }}
+
+
