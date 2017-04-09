2ef9044045daae5b20398aeecc3ebc90fc53ddfd
anastasia1296 <ivanko.anastasia@gmail.com>
2017-04-09 18:15:47 +0300

    some bad coomit with idea files

---
+++ src/Task8Additional.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by Anastasya on 09.04.2017.
+ */
+public class Task8Additional {
+    public static void main(String []args){
+        Scanner scan =new Scanner(System.in);
+        double purchasePrice= scan.nextDouble();
+        double discount = 0.1;
+        if(purchasePrice>1000){
+           double newPrice= purchasePrice*discount;
+           System.out.println(newPrice);
+        }else{
+            System.out.println("The price of purchase is not hight enought");
+        }
+    }
+}
