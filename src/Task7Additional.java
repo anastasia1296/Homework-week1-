4383f0c785b2ffc029541caa44db40d43c44cb36
anastasia1296 <ivanko.anastasia@gmail.com>
2017-04-09 17:57:25 +0300

    some bad coomit with idea files

---
+++ src/Task7Additional.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Created by Anastasya on 09.04.2017.
+ */
+public class Task7Additional {
+    public static void main (String[]args){
+        Scanner scan = new Scanner(System.in);
+        String [] month={"january","february","march","april","may","june","july","august","september","october",
+                "november", "december"};
+        System.out.println("Enter number of the month");
+        int number = scan.nextInt();
+            if(number>=1 && number<=month.length){
+                System.out.println("month-"+ month[number -1]);
+        }else {
+                System.out.println("Error");
+            }
+
+    }
+}
