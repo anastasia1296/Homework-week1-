9555536809db3cdb1a113f42de2bd2eae6fa2ee6
anastasia1296 <ivanko.anastasia@gmail.com>
2017-04-20 18:06:19 +0300

    some bad coomit with idea files

--- src/homework/week2/Task9.java
+++ src/homework/week2/Task9.java
@@ -1,7 +1,34 @@
 package homework.week2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 /**
  * Created by Anastasya on 17.04.2017.
  */
 public class Task9 {
-}
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        String [] timeTable = Files.lines(Paths.get("C:/Users/Anastasya/Desktop/TimeTable.txt"))
+                .toArray(size -> new String[size]);
+        System.out.println("Enter the day number of week");
+
+            int numberOfTheDay = scan.nextInt();
+            if (numberOfTheDay >= 1 && numberOfTheDay <= timeTable.length) {
+                System.out.println(timeTable[numberOfTheDay -1]);
+            } else if (numberOfTheDay == 0) {
+                System.out.println("Good bye!");
+                return;
+            } else {
+                System.out.println("Wrong input");
+            }
+
+
+        }
+
+    }
+
