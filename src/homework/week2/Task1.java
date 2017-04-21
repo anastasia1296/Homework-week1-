678278625d281730adf112716c3e2bf142debf8e
anastasia1296 <ivanko.anastasia@gmail.com>
2017-04-21 17:45:14 +0300

    some bad coomit with idea files

--- src/homework/week2/Task1.java
+++ src/homework/week2/Task1.java
@@ -9,15 +9,22 @@ public class Task1 {
     public static void main(String []args) {
         int[] arr = {5, 56, 90, 87, 66};
         int max = arr[0];
+        int imax =0;
+        int imin=0;
         int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
+                imax=i;
             }
             if (arr[i] < min) {
                 min = arr[i];
+                imin=i;
             }
-        }
-        System.out.println(max + ";" + min);
-    }}
+    }
+        arr[imin] = max;
+        arr[imax] = min;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print( arr[i]+"; ");
+    }}}
 
--- src/homework/week2/Task11.java
+++ src/homework/week2/Task11.java
@@ -8,6 +8,7 @@ public class Task11 {
         String sentence = new String("Spring day");
         System.out.println(sentence);
         System.out.println(change(sentence));
+        System.out.println( arrayReverse(sentence));
     }
     public static  String change (String sentence){
         String[] parts = sentence.split(" ");
@@ -21,5 +22,13 @@ public class Task11 {
         swapped += parts[0];
         return swapped;
     }
-}
+    public static char[] arrayReverse(String sentence){
+        char[] array = sentence.toCharArray();
+        System.out.print("Reverse: ");
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i]);
+    }
+        return array;
+
+}}
 
